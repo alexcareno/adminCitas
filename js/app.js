dfc6bce90f0fbf00ca7d8dda93cd50feba1838b8
Alejandro de Jesús Escareño Flores <alejandroef04@gmail.com>
2024-10-30 21:04:09 -0600

    🚧 Selecting inputs
    Starting the form validation

---
+++ js/app.js
@@ -0,0 +1,40 @@
+// Selectores
+const pacienteInput = document.querySelector('#paciente');
+const propietarioInput = document.querySelector('#propietario');
+const emailInput = document.querySelector('#email');
+const fechaInput = document.querySelector('#fecha');
+const sintomasInput = document.querySelector('#sintomas');
+
+const formulario = document.querySelector('#formulario-cita');
+
+// Objeto de Cita
+const citaObj = {
+    paciente: '',
+    propietario: '',
+    email: '',
+    fecha: '',
+    sintomas: ''
+}
+
+// Eventos
+pacienteInput.addEventListener('change', datosCita);
+propietarioInput.addEventListener('change', datosCita);
+emailInput.addEventListener('change', datosCita);
+fechaInput.addEventListener('change', datosCita);
+sintomasInput.addEventListener('change', datosCita);
+
+formulario.addEventListener('submit', submitCita);
+
+// Funciones
+function datosCita(e) {
+    citaObj[e.target.name] = e.target.value;
+}
+
+function submitCita(e) {
+    e.preventDefault();
+
+    if( Object.values(citaObj).some(valor => valor.trim() === '') ) {
+        console.log('Faltan datos');
+        return;
+    }
+}
